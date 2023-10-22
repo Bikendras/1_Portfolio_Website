c48b33d9ab7e738cbbe4501c0b2c68e4e14c2477
Bikendras <146335724+Bikendras@users.noreply.github.com>
2023-10-22 20:32:07 +0530

    add form component...

--- portfolio/src/Navbar.jsx
+++ portfolio/src/Navbar.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { FaGithubSquare,FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FaGithubSquare, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import { FaBars } from 'react-icons/fa';
 import { links, social } from './Data';
-import About from './About';
-import Skills from './Skills';
-import Projects from './Projects';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 
@@ -25,45 +22,43 @@ const Navbar = () => {
   }, [showLinks]);
   return (
     <div>
-        <div>
+      <div>
         <nav>
-      <div className='nav-center'>
-        <div className='nav-header'>
-            <span  className='Flipkart'>Port<span style={{color:"#059669"}}>folio</span></span>
-          <button className='nav-toggle' onClick={toggleLinks}>
-            <FaBars/>
-          </button>
-        </div>
-        <div className='links-container' ref={linksContainerRef}>
-          <ul className='links' ref={linksRef}>
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <ul className='social-icons'>
-          {social.map((socialIcon) => {
-            const { id, url, icon } = socialIcon;
-            return (
-              <li key={id}>
-                <a href={url}>{icon}</a>
-              </li>
-            );
-          })}
-        </ul>
+          <div className='nav-center'>
+            <div className='nav-header'>
+              <span className='Flipkart'>Port<span style={{ color: "#059669" }}>folio</span></span>
+              <button className='nav-toggle' onClick={toggleLinks}>
+                <FaBars />
+              </button>
+            </div>
+            <div className='links-container' ref={linksContainerRef}>
+              <ul className='links' ref={linksRef}>
+                {links.map((link) => {
+                  const { id, url, text } = link;
+                  return (
+                    <li key={id}>
+                      <a href={url}>{text}</a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <ul className='social-icons'>
+              {social.map((socialIcon) => {
+                const { id, url, icon } = socialIcon;
+                return (
+                  <li key={id}>
+                    <a href={url}>{icon}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </nav>
+        <Outlet />
       </div>
-    </nav>
-    <Outlet/>
+      <Footer />
     </div>
-      <Footer/>
-    </div>
-    
-    
   );
 };
 
